d405ce680f058d904ad54973fd2ba2ad6ea3f072
razorbanana <stanislav.stefyuk2@gmail.com>
2024-07-24 17:57:54 +0300

    feat: forms close on moving to other tabs

--- src/components/Topper.tsx
+++ src/components/Topper.tsx
@@ -5,28 +5,42 @@ import { hideDiaryForm } from "../app/forms/diaryFormSlice";
 import { hideEntryForm } from "../app/forms/entryFormSlice";
 import { hideEditUserForm } from "../app/forms/editUserFormSlice";
 import { ReturnButton } from "../components/ReturnButton";
+import { hideEditDiaryForm } from "../app/forms/editDiaryFormSlice";
+import { hideEditEntryForm } from "../app/forms/editEntryFormSlice";
+import { hideEditPasswordForm } from "../app/forms/editPasswordFormSlice";
 
 export const Topper = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
+
+    const closeForms = () => {
+        dispatch(hideEditDiaryForm())
+        dispatch(hideEditEntryForm())
+        dispatch(hideEditPasswordForm())
+        dispatch(hideDiaryForm())
+        dispatch(hideEntryForm())
+        dispatch(hideEditUserForm())
+    }
+
     return (
         <div className="Topper">
             <ReturnButton/>
             <TopperButton text="My Diaries" onClickFunction={() => {
+                closeForms()
                 navigate("/my-diaries")
             }}/>
             <TopperButton text="Home" onClickFunction={() => {
+                closeForms()
                 navigate("/home")
             }}/>
             <TopperButton text="Profile" onClickFunction={() => {
+                closeForms()
                 navigate("/profile")
             }}/>
             <TopperButton text="Log Out" onClickFunction={() => {
                 console.log("LogOut")
                 dispatch(setToken('undefined'))
-                dispatch(hideDiaryForm())
-                dispatch(hideEntryForm())
-                dispatch(hideEditUserForm())
+                closeForms()
                 localStorage.setItem('token', 'undefined')  
                 navigate("/my-diaries")  
             }}/>
